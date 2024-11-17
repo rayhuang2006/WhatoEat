79dc0f5f29027302d5c1511a2eb8dcada62e9acc
rayhuang2006 <rayhuang2006@gmail.com>
2024-11-17 16:11:09 +0800

    Initial Commit

---
+++ WhatoEat/WhatoEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatoEatApp.swift
+//  WhatoEat
+//
+//  Created by Ray Huang on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct WhatoEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhatoEatTests/WhatoEatTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatoEatTests.swift
+//  WhatoEatTests
+//
+//  Created by Ray Huang on 2024/11/17.
+//
+
+import Testing
+@testable import WhatoEat
+
+struct WhatoEatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
